7cd7c954ee8c20b34d07aac969022b2925844c87
ishaanchandola1234-tech <ishaanchandola1234@gmail.com>
2025-12-18 16:56:48 +0530

    app.py
    Fix routes and layout for continuous music

--- app.py
+++ app.py
@@ -121,6 +121,61 @@ function toggleMusic() {
 }
 </script>
 """
+BASE_HTML = """
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>For Hazel ❤️</title>
+{style}
+</head>
+
+<body>
+{audio}
+{content}
+</body>
+</html>
+"""
+
+
+
+@app.route("/start")
+def start():
+
+    met_days = (date.today() - date(2024, 9, 4)).days
+    dating_days = (date.today() - date(2024, 10, 8)).days
+
+
+    content = f"""
+    <div class="fade">
+        <h1>Our Beginning ❤️</h1>
+
+        <p>
+            We met on <b>4th September 2024</b><br>
+            And started dating on <b>8th October 2024</b>
+        </p>
+
+        <h2>⏳ {met_days} days since we met</h2>
+        <h2>💖 {dating_days} days since we became us</h2>
+
+        <div class="images">
+            <img src="/static/img1.jpg">
+            <img src="/static/img2.jpg">
+        </div>
+
+        <div class="nav">
+            <a href="/poem">Next ➜</a>
+        </div>
+    </div>
+    """
+
+    return render_template_string(
+        BASE_HTML.format(
+            style=STYLE,
+            audio=AUDIO,
+            content=content
+        )
+    )
 
 
 @app.route("/")
@@ -128,133 +183,37 @@ def home():
     met_days = (date.today() - date(2024, 9, 4)).days
     dating_days = (date.today() - date(2024, 10, 8)).days
 
+    content = f"""
+    <div class="fade">
+        <h1>Our Beginning ❤️</h1>
+
+        <p>
+            We met on <b>4th September 2024</b><br>
+            And started dating on <b>8th October 2024</b>
+        </p>
+
+        <h2>⏳ {met_days} days since we met</h2>
+        <h2>💖 {dating_days} days since we became us</h2>
+
+        <div class="images">
+            <img src="/static/img1.jpg">
+            <img src="/static/img2.jpg">
+        </div>
+
+        <div class="nav">
+            <a href="/poem">Next ➜</a>
+        </div>
+    </div>
+    """
 
     return render_template_string(
-f"""{STYLE}
-{AUDIO}
+        BASE_HTML.format(
+            style=STYLE,
+            audio=AUDIO,
+            content=content
+        )
+    )
 
-<div class="fade">
-    <h1>Our Beginning ❤️</h1>
-    <h2>⏳ {met_days} days since we met</h2>
-    <h2>💖 {dating_days} days since we became us</h2>
-
-
-    <p>
-        We met on <b>4th September 2024</b><br>
-        And started dating on <b>8th October 2024</b>
-    </p>
-
-    <div class="images">
-        <img src="/static/img1.jpg">
-        <img src="/static/img2.jpg">
-    </div>
-
-    <div class="nav">
-        <a href="/poem">Next ➜</a>
-    </div>
-</div>
-"""
-)
-
-@app.route("/poem")
-def poem():
-    return render_template_string(
-f"""{STYLE}
-{AUDIO}
-
-<div class="fade">
-    <h1>For You 💖</h1>
-
-    <div class="poem">
-The brown eyes ,
-The long hair
-
-The beautiful fragrance
-Is it what I fell for ?
-From considering love to be a distraction,
-To falling harder for you everyday ...
-From being an cold hearted boy ,
-To being a emotional soft hearted person ...
-From being an independent guy ,
-To being Totally dependent on your love
-Just the eyes , the hair , and the fragrance couldn't be all that I fell for ...
-
-Now that I think ,
-Now that I know ,
-It was something that was planned
-I think they call this destiny !
-You came like a bright light into my dull life
-You came like a ray of hope into my hopeless mind
-You came like a blessing ....
-You were the answer to my prayers ..
-YOU are what I fell for ...
-
-You are my home
-My safe place , my peace
-You are my strength ..
-For you I would cross a 1000 miles
-There are no boundaries
-I will always be there ,
-To protect you ,
-To guide you
-To support you ..
-I'll be your shadow
-You will never be alone ...
-
-I love you ..
-    </div>
-
-    <div class="images">
-        <img src="/static/img3.jpg">
-        <img src="/static/img4.jpg">
-    </div>
-
-    <div class="nav">
-        <a href="/">⬅ Back</a>
-        <a href="/poem2">Next ➜</a>
-    </div>
-</div>
-"""
-)
-@app.route("/poem2")
-def poem2():
-    return render_template_string(
-f"""{STYLE}
-{AUDIO}
-
-<div class="fade">
-    <h1>Always Us ✨</h1>
-
-    <div class="poem">
-Hazel
-Remember ? The time we first met ..
-Remember ? The blue lagoon ..
-Remember? How you were trying to comfort me when I was all shy
-Remember ? The note I gave u and left without even saying goodbye
-
-Because I remember,
-I remember the time i first saw your eyes
-I remember getting lost again and again being around you
-I remember how the world became silent for some time when you were around me
-I remember the blue lagoon
-I remember how I just gave you the note and left without saying goodbye ,
-just because I didn't want to say goodbye , hoping we would meet again
-It's been a year remember ?
-    </div>
-
-    <div class="images">
-        <img src="/static/img5.jpg">
-        <img src="/static/img6.jpg">
-    </div>
-
-    <div class="nav">
-    <a href="/poem">⬅ Back</a>
-    <a href="/promise">Next ➜</a>
-</div>
-
-</div>
-"""
-)
 
 @app.route("/promise")
 def promise():
@@ -298,6 +257,3 @@ if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-
-
-
